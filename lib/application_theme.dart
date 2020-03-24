104ee6261467d608d30b305ca87cbc7716148ead
Cortrano <61734748+Cortrano@users.noreply.github.com>
2020-03-24 19:52:42 +0300

    Updated color scheme

--- lib/application_theme.dart
+++ lib/application_theme.dart
@@ -4,11 +4,11 @@ class ApplicationThemeData {
   static ThemeData themeData() {
     return new ThemeData(
       brightness: Brightness.light,
-      primaryColorDark: Color(0xFF5D4037),
-      primaryColorLight: Color(0xFFD7CCC8),
-      primaryColor: Color(0xFF795548),
-      accentColor: Color(0xFFFFC107),
-      dividerColor: Color(0xFFBDBDBD),
+      primaryColorDark: Colors.deepPurple,
+      primaryColorLight: Colors.deepPurpleAccent,
+      primaryColor: Colors.deepPurple,
+      accentColor: Colors.orangeAccent,
+      dividerColor: Colors.black,
     );
   }
 }
